b885902ed9136f41f787604738de08c4201bcc79
John Papa <john@johnpapa.net>
2021-03-09 11:33:28 -0500

    starter function fixed

--- products-get/index.js
+++ products-get/index.js
@@ -1,10 +1,13 @@
-const data = require('../product-data');
+(module.exports = async function (context, req) {
+  context.log("JavaScript HTTP trigger function processed a request.");
 
-module.exports = async function (context, req) {
-  try {
-    const products = data.getProducts();
-    context.res.status(200).json(products);
-  } catch (error) {
-    context.res.status(500).send(error);
-  }
-};
+  const name = req.query.name || (req.body && req.body.name);
+  const responseMessage = name
+    ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+    : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    "body": responseMessage
+  };
+})
